bb91bd39070b853457501d2d00f62dcb1bc5c553
Alal Hosen <alalhosen1992620120@gmail.com>
2025-04-29 23:34:54 +0600

    lorem add in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-           <p>home</p> 
+           <p>Home! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptates voluptatum facere dolor obcaecati voluptas. Fuga nemo neque eaque laboriosam.</p> 
         </div>
     );
 };
